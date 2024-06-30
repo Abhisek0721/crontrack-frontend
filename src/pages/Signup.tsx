f763b9aaa2bfa9eae513dfe7e0cb6d05362b40c5
rahul kumar <142343501+CodeSciRahul@users.noreply.github.com>
2024-06-30 23:11:09 +0530

    feat: login/singup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,79 +1,91 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom"
-import Creato from "../assets/Creato-logo.jpg"
-import Background from "../assets/Login-Background.png"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+import Creato from "../assets/Creato-logo.jpg";
+import Background from "../assets/Login-Background.png";
 
 export function Signup() {
-    return (
-        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] overflow-hidden">
-        <div className="flex flex-col items-center justify-center pt-12 overflow-hidden">
-            <div className="flex justify-center items-center"> 
-                <img src={Creato} className=" w-[150px] rounded-[50%]" alt="" />
-            </div>
-            <div className="mx-auto grid w-[350px] gap-6">
-                <div className="grid gap-2 text-center">
-                    <h1 className="text-3xl font-bold">Signup</h1>
-                    <p className="text-balance text-muted-foreground">
-                        Enter your name and email below to create your account
-                    </p>
-                </div>
-                <form>
-                    <div className="grid gap-4">
-                        <div className="grid gap-2">
-                            <Label htmlFor="name">Name</Label>
-                            <Input
-                                id="name"
-                                type="text"
-                                placeholder="John"
-                                required
-                                autoComplete="username"
-                            />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="email">Email</Label>
-                            <Input
-                                id="email"
-                                type="email"
-                                placeholder="m@example.com"
-                                required
-                                autoComplete="username"
-                            />
-                        </div>
-                        <div className="grid gap-2">
-                            <div className="flex items-center">
-                                <Label htmlFor="password">Password</Label>
-                                <Link
-                                    to="#"
-                                    className="ml-auto inline-block text-sm underline"
-                                >
-                                    Forgot your password?
-                                </Link>
-                            </div>
-                            <Input id="password" type="password" name="password" required autoComplete="current-password" />
-                        </div>
-                        <Button type="submit" className="w-full">
-                            Signup
-                        </Button>
-                    </div>
-                </form>
-                <div className="mt-4 text-center text-sm">
-                    Have an account? {" "}
-                    <Link to="/login" className="underline">
-                        login
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="w-full flex justify-between items-center overflow-hidden">
+      <div className="fixed top-1 left-4">
+        {" "}
+        <img src={Creato} className=" w-[80px] rounded-[50%]" alt="" />
+      </div>
+
+      <div className="lg:min-w-[30%] my-auto mx-auto px-3">
+        <div className="text-center py-6">
+          <h1 className="text-3xl font-bold py-4">Signup</h1>
+          <p className="text-balance text-muted-foreground">
+            Set up your account to start using Creato
+          </p>
         </div>
-        <div className="hidden bg-muted lg:block w-full overflow-hidden">
-            <img
-                src={Background}
-                alt="Image"
-                className="w-full  object-cover dark:brightness-[0.2] dark:grayscale"
-            />
+
+        <form className="py-4">
+          <div className="grid gap-5">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Username</Label>
+              <Input
+                id="Username"
+                type="Username"
+                placeholder="john_doe"
+                required
+                autoComplete="username"
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                autoComplete="username"
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input
+                id="password"
+                type="password"
+                name="password"
+                required
+                autoComplete="current-password"
+              />
+              <Link
+                to="#"
+                className=" mt-[-0.5rem] inline-block text-sm text-muted-foreground"
+              >
+                Forgot your password?
+              </Link>
+            </div>
+            <Button type="submit" className="w-full bg-[#0e263e]">
+              Signup
+            </Button>
+            <Button variant="outline" className="w-full">
+              Sign up with Google
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-4 text-center text-sm">
+          have an account?{" "}
+          <Link to="/login" className="underline">
+            login
+          </Link>
         </div>
+      </div>
+
+      <div className="hidden lg:block overflow-hidden w-[60%] h-[100vh] bg-[#001F3F]">
+        <img
+          src={Background}
+          alt="Background Image"
+          className="dark:brightness-[0.2] dark:grayscale w-full h-full object-contain"
+          loading="lazy"
+        />
+      </div>
     </div>
-    
-    )
+  );
 }
