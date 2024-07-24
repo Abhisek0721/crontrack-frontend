c1b60b16a684ee9d991c41cdc2cffbbb925d9cd6
rahul kumar <142343501+CodeSciRahul@users.noreply.github.com>
2024-07-25 00:29:02 +0530

    feat: Add dashboard

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,41 +1,30 @@
-import { Metadata } from "next"
-import Image from "next/image"
 import { PlusCircledIcon } from "@radix-ui/react-icons"
 
 import { Button } from "@/components/ui/button"
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 import { Separator } from "@/components/ui/separator"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
+import { TabsContent,Tabs,TabsList,TabsTrigger } from "@/components/ui/tabs"
 
-import { AlbumArtwork } from "./components/album-artwork"
-import { Menu } from "./components/menu"
-import { PodcastEmptyPlaceholder } from "./components/podcast-empty-placeholder"
-import { Sidebar } from "./components/sidebar"
-import { listenNowAlbums, madeForYouAlbums } from "./data/albums"
-import { playlists } from "./data/playlists"
+import { AlbumArtwork } from "@/components/album-artwork"
+import { Menu } from "@/components/menu"
+import { PodcastEmptyPlaceholder } from "@/components/podcast-empty-placeholder"
+import { Sidebar } from "@/components/sidebar"
+import { listenNowAlbums, madeForYouAlbums } from "../DummyData/albums"
+import { playlists } from "../DummyData/playlists"
 
-export const metadata: Metadata = {
-  title: "Music App",
-  description: "Example music app using the components.",
-}
 
 export default function MusicPage() {
   return (
     <>
       <div className="md:hidden">
-        <Image
+        <img
           src="/examples/music-light.png"
           width={1280}
           height={1114}
           alt="Music"
           className="block dark:hidden"
         />
-        <Image
+        <img
           src="/examples/music-dark.png"
           width={1280}
           height={1114}
