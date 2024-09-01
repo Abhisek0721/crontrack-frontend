b70f2de00f5f7feb5eee9800c7ffb0fb2ef668d1
rahul kumar <142343501+CodeSciRahul@users.noreply.github.com>
2024-09-01 23:18:45 +0530

    feat : write soft for base url

--- src/Redux/api/apiSlice.ts
+++ src/Redux/api/apiSlice.ts
@@ -2,11 +2,11 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 import { getToken } from "../util/getUserDetailFromBrowser";
 import 'dotenv'
 
-
+console.log("base_url",  import.meta.env.VITE_API_URL);
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3001",
+        baseUrl: import.meta.env.VITE_API_URL,
         prepareHeaders: (header) => {
             const token = getToken();
             if(token) {
