c9e245cc7cb2e0bc58882fb5022288e209e2b62c
rahul kumar <142343501+CodeSciRahul@users.noreply.github.com>
2024-09-25 20:57:13 +0530

    feat : add invite member

--- src/components/workSpaceSwitcher.tsx
+++ src/components/workSpaceSwitcher.tsx
@@ -18,20 +18,36 @@ import {
   CommandItem,
 
 } from "@/components/ui/command";
+
 import { useAppSelecter } from "../Redux/Hooks/store";
+import { useAppDispatch } from "../Redux/Hooks/store";
+import { selectedWorkspace } from "../Redux/feature/authSlice";
 import { useState } from "react";
 
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
+import { useNavigate } from "react-router-dom";
 
 export const WorkSpaceSwitcher = ({
   className,
 }: React.HTMLAttributes<HTMLElement>) => {
-  const userWorkSpaceSpaces = useAppSelecter(
+  const userWorkSpaces = useAppSelecter(
     (state) => state.auth.user_workspace
   );
-  console.log(userWorkSpaceSpaces);
-  const [firstWorkspace, setfirstWorkspace] = useState<string>("workspace");
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const workspace:string = userWorkSpaces? userWorkSpaces[0]?.workspace?.workspace_name: "workspace";
+  const [firstWorkspace, setfirstWorkspace] = useState<string>(`${workspace}`);
+
+  const select_Workspace = userWorkSpaces?.find((workspace) => {
+    return workspace?.workspace?.workspace_name === `${firstWorkspace}`;
+  })
+
+  select_Workspace && dispatch(selectedWorkspace(select_Workspace));
+
+  console.log(selectedWorkspace)
+
 
   return (
     <>
@@ -49,7 +65,7 @@ export const WorkSpaceSwitcher = ({
               <CommandList>
                 <CommandEmpty>No Workspace found</CommandEmpty>
                 <CommandGroup heading="Your Workspace">
-                  {userWorkSpaceSpaces?.map((workspace) => (
+                  {userWorkSpaces?.map((workspace) => (
                     <CommandItem
                       key={workspace?.id}
                       className="flex justify-between cursor-pointer"
@@ -71,7 +87,7 @@ export const WorkSpaceSwitcher = ({
               </CommandList>
             </Command>
 
-            <Button variant="ghost" className="flex gap-2 w-full">
+            <Button variant="ghost" className="flex gap-2 w-full" onClick={() => navigate("/create-workspace-name")}>
               <PlusCircledIcon className="" /> Create Workspace
             </Button>
           </PopoverContent>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,11 @@
 export const constant = {
     API_URL: import.meta.env.VITE_API_URL,
     APP_NAME: import.meta.env.VITE_APP_NAME || 'Crontrack',
+    role: {
+        admin: 'admin',
+        manager: "manager",
+        contentCreator: "content-creator",
+        analyst: "analyst",
+        viewer: "viewer"
+    }
 }
