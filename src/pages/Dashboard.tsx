3cb23d9be363243c6fe489a5d613ffb1eac8c698
= <=>
2024-12-24 14:48:06 +0530

    refactor: refactor dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,4 @@
 import { Separator } from "@/components/ui/separator";
-import { TabsContent, Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { TopNavBar } from "../components/TopNavBar";
 import { Sidebar } from "../components/Sidebar";
 import { BottomNavbar } from "../components/BottomNavbar";
@@ -27,86 +26,24 @@ export default function Dashboard() {
 
   return (
     <>
-      <div className="">
+     <div className="h-screen flex flex-col">
         <ProgressMessage
           message="This page is under construction. Please check back soon"
           className=""
         />
         <TopNavBar />
-        <div className="border-t">
-          <div className="bg-background">
-            <div className="grid lg:grid-cols-5">
-              <Sidebar className="hidden lg:block" />
-              <div className="col-span-3 lg:col-span-4 lg:border-l">
-                <div className="h-full px-4 py-6 lg:px-8">
-                  <Tabs
-                    defaultValue="upcoming post"
-                    className="h-full space-y-6"
-                  >
-                    <div className="space-between flex items-center">
-                      <TabsList>
-                        <TabsTrigger value="upcoming post" className="relative">
-                          Upcoming Post
-                        </TabsTrigger>
-                        <TabsTrigger value="previous post">
-                          Previous Post
-                        </TabsTrigger>
-                        <TabsTrigger value="live">Live</TabsTrigger>
-                      </TabsList>
-                    </div>
 
-                    {/* Upcoming Post ke andar dikhne wala ui  */}
-                    <TabsContent
-                      value="upcoming post"
-                      className="h-full flex-col border-none p-0 data-[state=active]:flexe"
-                    >
-                      <div className="flex items-center justify-between">
-                        <div className="space-y-1">
-                          <p className="text-sm text-muted-foreground">
-                            Your Upcoming post.
-                          </p>
-                        </div>
-                      </div>
-                      <Separator className="my-4" />
-                    </TabsContent>
+        <Separator orientation="horizontal" className="w-full" />
 
-                    {/* Previous Post ke andar dikhne wala ui  */}
-                    <TabsContent
-                      value="previous post"
-                      className="h-full flex-col border-none p-0 data-[state=active]:flex"
-                    >
-                      <div className="flex items-center justify-between">
-                        <div className="space-y-1">
-                          <p className="text-sm text-muted-foreground">
-                            Your previous post.
-                          </p>
-                        </div>
-                      </div>
-                      <Separator className="my-4" />
-                    </TabsContent>
-
-                    {/* Live ke andar dikhne wala ui  */}
-                    <TabsContent
-                      value="live"
-                      className="h-full flex-col border-none p-0 data-[state=active]:flex"
-                    >
-                      <div className="flex items-center justify-between">
-                        <div className="space-y-1">
-                          <p className="text-sm text-muted-foreground">
-                            You are live
-                          </p>
-                        </div>
-                      </div>
-                      <Separator className="my-4" />
-                    </TabsContent>
-                  </Tabs>
-                </div>
-              </div>
-            </div>
+        <div className="flex flex-1 bg-background">
+          <Sidebar className="hidden lg:block h-full w-96" />
+          <Separator orientation="vertical" className="hidden lg:block h-full" />
+          <div className="flex-grow p-4 w-full">
+            <Outlet />
           </div>
         </div>
+
         <BottomNavbar className="block lg:hidden" />
-        <Outlet />
       </div>
     </>
   );
